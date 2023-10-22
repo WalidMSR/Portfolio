88a39916f6cd60d0eebe7c5cbaf76f0201ca63ab
WISSAL MN <135167709+WissalManseri@users.noreply.github.com>
2023-10-22 15:45:15 +0100

    Define the URL patterns for your views, including download and view report URLs.

--- cv/urls.py
+++ cv/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
 
-
-urlpatterns =[
+urlpatterns = [
     path('', views.index, name='index'),
     path('download_report/<int:project_id>/', views.download_report, name='download_report'),
+    path('view_report/<int:project_id>/', views.view_report, name='view_report'),
 ]
